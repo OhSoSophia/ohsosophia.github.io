d8bc0e6a6b90f2a0629e23983dace4074a58d8cd
Sophia Lee <sophialee@college.harvard.edu>
2018-07-23 16:34:27 -0400

    basic templates

--- application.py
+++ application.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+@app.route('/')
+def index():
+    """ open homepage """
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
